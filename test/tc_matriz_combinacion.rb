b49e7a59fbacc9b4974c56fd1edb7d220034f59b
Sergio Diaz <alu0100696615@ull.edu.es>
2013-12-12 20:00:31 +0000

    Hecho test matriz combinación

--- test/tc_matriz_combinacion.rb
+++ test/tc_matriz_combinacion.rb
@@ -1,7 +1,7 @@
 require "prct12"
 require "test/unit"
 
-#Clase para devolver un cero en matrices densas de enteros.
+#Devuelve un cero en matrices densas de enteros.
 class MatrizDensaFixnum < Prct12::MatrizDensa
     #Devuelve un cero.
     def zero
@@ -9,7 +9,7 @@ class MatrizDensaFixnum < Prct12::MatrizDensa
     end
 end
 
-#Clase para devolver un cero en matrices densas de fracciones.
+#Devuelve un cero en matrices densas de fracciones.
 class MatrizDensaFraccion < Prct12::MatrizDensa
     #Devuelve un cero.
     def zero
@@ -17,7 +17,7 @@ class MatrizDensaFraccion < Prct12::MatrizDensa
     end
 end
 
-#Clase para devolver un cero en matrices dispersas de enteros.
+#Devuelve un cero en matrices dispersas de enteros.
 class MatrizDispersaFixnum < Prct12::MatrizDispersa
     #Devuelve un cero.
     def zero
@@ -25,7 +25,7 @@ class MatrizDispersaFixnum < Prct12::MatrizDispersa
     end
 end
 
-#Clase para devolver un cero en matrices dispersas de fracciones.
+#Devuelve un cero en matrices dispersas de fracciones.
 class MatrizDispersaFraccion < Prct12::MatrizDispersa
     #Devuelve un cero.
     def zero
@@ -33,56 +33,55 @@ class MatrizDispersaFraccion < Prct12::MatrizDispersa
     end
 end
 
-#Clase para probar operaciones entre matrices densas y dispersas.
+#Diferentes operaciones con matrices dispersas y densas
 class Test_Matriz_Combinacion < Test::Unit::TestCase
 
     #Crear e inicializar matrices.
     def setup
-        # Crear matrices
+        # Matrices 2x2
         @m1 = MatrizDensaFixnum.new(2, 2)
         @m2 = MatrizDispersaFixnum.new(2, 2)
         @m3 = MatrizDensaFixnum.new(2, 2)
 
         # Inicializar
-        # --Enteras
-        @m1[0, 0] = 2
-        @m1[0, 1] = 4
-        @m1[1, 0] = 1
-        @m1[1, 1] = 6
+        @m1[0, 0] = 1
+        @m1[0, 1] = 2
+        @m1[1, 0] = 3
+        @m1[1, 1] = 4
 
         @m2[0, 0] = 0
         @m2[0, 1] = 0
-        @m2[1, 0] = 3
+        @m2[1, 0] = 2
         @m2[1, 1] = 0
     end
 
     #Pruebas con enteros.
     def test_enteros
         #Suma
-        @m3[0, 0] = 2
-        @m3[0, 1] = 4
-        @m3[1, 0] = 4
-        @m3[1, 1] = 6
+        @m3[0, 0] = 1
+        @m3[0, 1] = 2
+        @m3[1, 0] = 5
+        @m3[1, 1] = 4
         assert_equal(@m3, @m1+@m2)
 
         #Resta
-        @m3[0, 0] = 2
-        @m3[0, 1] = 4
-        @m3[1, 0] = -2
-        @m3[1, 1] = 6
+        @m3[0, 0] = 1
+        @m3[0, 1] = 2
+        @m3[1, 0] = 1
+        @m3[1, 1] = 4
         assert_equal(@m3, @m1-@m2)
 
         #Multiplicación
-        @m3[0, 0] = 12
+        @m3[0, 0] = 0
         @m3[0, 1] = 0
-        @m3[1, 0] = 18
+        @m3[1, 0] = 6
         @m3[1, 1] = 0
         assert_equal(@m3, @m1*@m2)
 
         #Comparación
         @m3[0, 0] = 0
         @m3[0, 1] = 0
-        @m3[1, 0] = 3
+        @m3[1, 0] = 2
         @m3[1, 1] = 0
         assert_equal(@m3, @m2)
     end
