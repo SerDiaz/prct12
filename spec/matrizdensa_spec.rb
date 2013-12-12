a342a54f882ec9b044384bd4d9ca01c5d19a3dd2
Sergio Diaz <alu0100696615@ull.edu.es>
2013-12-12 18:16:33 +0000

    Añadida la prueba para fracciones

--- spec/matrizdensa_spec.rb
+++ spec/matrizdensa_spec.rb
@@ -95,4 +95,72 @@ describe Prct12::MatrizDensa do
       (tmp.encontrar{|e| e*e >= 16}).should eq([1, 0])
     end
   end
+#-----------------------------------------------------------------------
+  
+  
+  describe "Fracciones" do
+    before :all do
+      # Creando matrices 2x2
+      # La variable @m3 recordamos será el resultado
+      @m1 = MatrizDensaFraccion.new(2, 2)
+      @m2 = MatrizDensaFraccion.new(2, 2)
+      @m3 = MatrizDensaFraccion.new(2, 2)
+
+      @m1[0, 0] = Prct12::Fraccion.new(1, 1)
+      @m1[0, 1] = Prct12::Fraccion.new(2, 2)
+      @m1[1, 0] = Prct12::Fraccion.new(1, 2)
+      @m1[1, 1] = Prct12::Fraccion.new(3, 2)
+
+      @m2[0, 0] = Prct12::Fraccion.new(1, 3)
+      @m2[0, 1] = Prct12::Fraccion.new(2, 1)
+      @m2[1, 0] = Prct12::Fraccion.new(1, 1)
+      @m2[1, 1] = Prct12::Fraccion.new(2, 3)
+    end
+
+
+
+    it "Suma" do
+      @m3[0, 0] = Prct12::Fraccion.new(4, 3)
+      @m3[0, 1] = Prct12::Fraccion.new(3, 1)
+      @m3[1, 0] = Prct12::Fraccion.new(3, 2)
+      @m3[1, 1] = Prct12::Fraccion.new(13, 6)
+
+      (@m1+@m2).should eq(@m3)
+    end
+
+    it "Resta" do
+      @m3[0, 0] = Prct12::Fraccion.new(2, 3)
+      @m3[0, 1] = Prct12::Fraccion.new(-2, 2)
+      @m3[1, 0] = Prct12::Fraccion.new(-1, 2)
+      @m3[1, 1] = Prct12::Fraccion.new(5, 6)
+
+      (@m1-@m2).should eq(@m3)
+    end
+
+    it "Multiplicación" do
+      @m3[0, 0] = Prct12::Fraccion.new(1, 3)
+      @m3[0, 1] = Prct12::Fraccion.new(2, 1)
+      @m3[1, 0] = Prct12::Fraccion.new(1, 2)
+      @m3[1, 1] = Prct12::Fraccion.new(1, 1)
+
+      (@m1*@m2).should eq(@m3)
+    end
+
+    it "Comparación" do
+      @m3[0, 0] = Prct12::Fraccion.new(1, 1)
+      @m3[0, 1] = Prct12::Fraccion.new(2, 2)
+      @m3[1, 0] = Prct12::Fraccion.new(1, 2)
+      @m3[1, 1] = Prct12::Fraccion.new(3, 2)
+
+      @m1.should eq(@m3)
+    end
+    it "Máximo" do
+      (@m1.max).should eq(Prct12::Fraccion.new(3, 2))
+    end
+    it "Mínimo" do
+      (@m1.min).should eq(Prct12::Fraccion.new(1, 2))
+    end
+  end
+
+  
 
